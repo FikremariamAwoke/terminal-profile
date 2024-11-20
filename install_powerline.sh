00c7c7f77723d1080b0f2a74de0fc1162e3196e8
Fikremariam <fikrea197@gmail.com>
2024-11-20 15:48:08 +0300

    fix: fixed powerline global installation error

--- install_powerline.sh
+++ install_powerline.sh
@@ -3,7 +3,7 @@ set -eux pipefail
 
 # Install Powerline for VIM.
 sudo apt install -y python3-pip
-pip3 install --user powerline-status
+sudo apt install python3-powerline
 sudo cp configs/.vimrc ~/.vimrc
 sudo apt install -y fonts-powerline
 
